d3936dd587d875766da4d826bf06a0f5dfcc5cf9
Artur Shamsiev <arthan1011@gmail.com>
2015-12-17 17:17:14 +0300

    jms almost done

---
+++ jms/ticket-agency-jms/src/main/java/org/arthan/jms/jms/MDBService.java
@@ -0,0 +1,41 @@
+package org.arthan.jms.jms;
+
+import org.jboss.ejb3.annotation.ResourceAdapter;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.inject.Inject;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import java.util.logging.Logger;
+
+/**
+ * Created by ashamsiev on 17.12.2015
+ */
+
+@MessageDriven(name = "MDBService", activationConfig = {
+        @ActivationConfigProperty(
+                propertyName = "destination",
+                propertyValue = "java:jboss/activemq/queue/TicketQueue"
+        ),
+        @ActivationConfigProperty(
+                propertyName = "destinationType",
+                propertyValue = "javax.jsm.Queue"
+        )
+})
+@ResourceAdapter(value = "activemq-rar-5.9.0.rar")
+public class MDBService implements MessageListener {
+
+    @Inject
+    Logger logger;
+
+    public void onMessage(Message message) {
+        try {
+            final String text = message.getBody(String.class);
+            logger.info("Received message: " + text);
+        } catch (JMSException e) {
+            logger.severe(e.toString());
+        }
+    }
+}
